308979422de6420ee188f512b03813f177d0c7ba
James Pavur <pavja2@gmail.com>
2015-02-07 00:55:47 -0500

    Propel Testing

--- index.php
+++ index.php
@@ -2,9 +2,13 @@
 
 // put full path to Smarty.class.php
 require('/var/www/smarty/Smarty.class.php');
+require_once '/var/www/vendor/autoload.php';
+
+// setup Propel
+require_once '/var/www/vendor/propel/schemas/generated-conf/config.php';
 $smarty = new Smarty();
+
 
 $smarty->assign('name', 'Ned');
 $smarty->display('index.tpl');
-
 ?>
