3ac8cad4317413e4c10cf29e74f65c4e54157a0d
James Pavur <pavja2@gmail.com>
2015-02-07 01:00:34 -0500

    Propel Test!

--- index.php
+++ index.php
@@ -6,9 +6,12 @@ require_once '/var/www/vendor/autoload.php';
 
 // setup Propel
 require_once '/var/www/vendor/propel/schemas/generated-conf/config.php';
+$user = new User();
+$user->setUserName('Name');
+$user->save();
+
+
 $smarty = new Smarty();
-
-
 $smarty->assign('name', 'Ned');
 $smarty->display('index.tpl');
 ?>
